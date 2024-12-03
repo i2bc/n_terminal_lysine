aec64c684da40929009de43656da85ae0f6344b4
clairetn <claire.ctn@gmail.com>
2024-12-03 15:25:54 +0100

    add OGT & temp in title of aa graphs (frequency x position)

--- aaFreqByPos.R
+++ aaFreqByPos.R
@@ -4,17 +4,18 @@ library(ggplot2)
 # get data from command line:
 args <- commandArgs(trailingOnly=TRUE)
 # args affectations for testing into interactive R :
-# args <- c("-i", "GCF_000005845.2.csv", "-s", "E.coli")
+# args <- c("-i", "GCA_900095815.1.csv", "-s", "Methanothermobacter wolfeii ATCC43096", "-t", "(55.0°C, 1792 CDS)")
 if (length(args)==0) {
-  stop("Arguments to be supplied: -i inputFile.csv -s \"speciesNameForGarphTitle\"", call.=FALSE)
+  stop("Arguments to be supplied: -i inputFile.csv -s \"speciesNameForGarphTitle\" -t \"OgtAndTempForGarphTitle\"", call.=FALSE)
 } else {
   for (i in seq(1,length(args))) {
     if(args[i]=="-i") { inFile <- args[i+1] } 
     if(args[i]=="-s") {species <- args[i+1] }
+    if(args[i]=="-t") {ogtTemp <- args[i+1] }
   }
 }
 # output definitions:
-graphTitle <- paste(species)
+graphTitle <- paste(species,"\n",ogtTemp)
 position <- seq(1,20) # add a "position" column to graphical managment of positions
  
 # get data:
@@ -30,7 +31,7 @@ aaGraph <- ggplot(cbind(position,aa),aes(x=position))+
    geom_line(aes(y=R,color="R"),linewidth=1)+ # blue #4477AA
    geom_line(aes(y=Y,color="Y"))+ # #AA3377
    scale_color_manual(values=c("#CCBB44","darkorange","red","green4","blue","#AA3377"))+
-   labs(title = graphTitle, x = "aa position", y = "aa count", color="legend")+
+   labs(title = graphTitle, x = "aa position", y = "aa frequency", color="legend")+
    theme_bw()
 
 # graph save (pdf and png formats):
--- prepare_graphs.sh
+++ prepare_graphs.sh
@@ -6,12 +6,15 @@
 # $3: number of aa positions
 #
 # command line eg.:
-# bash prepare_graphs.sh *.list n_terminal_lysine-main 20
+# bash prepare_graphs.sh species_Archaea n_terminal_lysine 20
 #
-rm ${1}.KRNHYIaa_aaFreqByPos_graphes.sh
-for SP in `cut -f1 ${1} ` ; do
+rm graph_${1}_tmp.sh ; 
+for SP in `cut -f1 ${2}/${1}.list ` ; do
    # ${SP/.*/\*}.prophecy suppress sequenceID version number if present
    # the sumByPos variable containt the number of aa by position and is used to compute percentages from counts
    tail -n+10 Tmp/${SP/.*/\*}.prophecy | head -n ${3} | awk '{sumByPos=0; for(i=1;i<=NF;i++){sumByPos=sumByPos+$i}; for(i=1;i<=NF-1;i++){printf("%1.2f ",$i*100/sumByPos)}; printf("%1.2f\n",$NF*100/sumByPos)}'| awk 'BEGIN{print "K,R,N,H,Y,I";OFS=","}{print $11,$18,$14,$8,$25,$9}' > Tmp/KRNHYIaa_${SP}.csv ;
-   awk -v sp=${SP} -v tools=${2} -F "\t" '{if($1==sp){print "Rscript "tools"/aaFreqByPos.R -i Tmp/KRNHYIaa_"sp".csv -s \""$2"\""}}' ${1} >> ${1}.KRNHYIaa_aaFreqByPos_graphes.sh ; 
+   grep ${SP/.*/\*} n_terminal_lysine/OGT_nbCDS.tsv | awk -v tools="n_terminal_lysine" -v sp=${SP} -F "\t" '{print "Rscript "tools"/aaFreqByPos.R -i Tmp/KRNHYIaa_"sp"*.csv -s \""$2"\" -t ,"$3","$4}' >> graph_${1}_tmp.sh ; 
 done
+sort -t "," -nk2,2 graph_${1}_tmp.sh | sed 's/,/"(OGT: /;s/,/°C, /;s/$/ CDS)"/' > graph_${1}.sh
+bash graph_${1}.sh
+
